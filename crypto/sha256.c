75baa824395da961ace80831d3911114b7b970be
Anas Trak <anastarak9@gmail.com>
2021-04-06 00:53:29 +0100

    inshaellah

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -2,14 +2,10 @@
 uint8_t * sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH] )
 {
 	SHA256_CTX sha_ctx;
-	
 	if(digest == NULL)
 		return NULL;
-		
 	SHA256_Init(&sha_ctx);
 	SHA256_Update(&sha_ctx,s,len);
 	SHA256_Final(digest,&sha_ctx);
-	
-	return digest;	
+	return digest;
 }
-
