84a116a2becc0acb5d55d99a7f7a229fb672111b
Anas Trak <anastarak9@gmail.com>
2021-04-06 02:10:46 +0100

    inshaellah works

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,40 +1,28 @@
-#ifndef HBLK_CRYPTO
-#define HBLK_CRYPTO
+#ifndef HBLK_CRYPTO_H
+#define HBLK_CRYPTO_H
 
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <sys/stat.h>
-#include <sys/types.h>
-
-#include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/evp.h>
-/* #include <openssl/bn.h> */
-#include <openssl/bio.h>
-#include <openssl/err.h>
-#include <openssl/ssl.h>
 #include <openssl/pem.h>
+#include <openssl/sha.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/stat.h>
 
-#define PUB_FILENAME "key_pub.pem"
-#define PRI_FILENAME "key.pem"
-
-# define EC_CURVE   NID_secp256k1
+#define PUB_FILENAME	"key_pub.pem"
+#define PRI_FILENAME	"key.pem"
+#define EC_CURVE	NID_secp256k1
 
 /* EC_KEY public key octet string length (using 256-bit curve) */
-# define EC_PUB_LEN 65
+#define EC_PUB_LEN	65
 /* Maximum signature octet string length (using 256-bit curve) */
-# define SIG_MAX_LEN    72
+#define SIG_MAX_LEN	72
 
 /**
  * struct sig_s - EC Signature structure
  *
  * @sig: Signature buffer. The whole space may not be used
- * @len: Actual signature size. Can't exceed SIG_MAX_LEN,
- * therefore stored on a byte
+ * @len: Signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
  */
 typedef struct sig_s
 {
@@ -42,27 +30,20 @@ typedef struct sig_s
 	 * @sig must stay first, so we can directly use the structure as
 	 * an array of char
 	 */
-	uint8_t     sig[SIG_MAX_LEN];
-	uint8_t     len;
+	uint8_t	sig[SIG_MAX_LEN];
+	uint8_t	len;
 } sig_t;
 
 uint8_t* sha256(int8_t const* s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH]);
-
 EC_KEY* ec_create(void);
-
 uint8_t* ec_to_pub(EC_KEY const* key, uint8_t pub[EC_PUB_LEN]);
-
 EC_KEY* ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
-
 int ec_save(EC_KEY* key, char const* folder);
-
 EC_KEY* ec_load(char const* folder);
-
 uint8_t* ec_sign(EC_KEY const* key, uint8_t const* msg, size_t msglen,
 	sig_t* sig);
-
 int ec_verify(EC_KEY const* key, uint8_t const* msg, size_t msglen,
 	sig_t const* sig);
 
-#endif
+#endif /* HBLK_CRYPTO_H */
