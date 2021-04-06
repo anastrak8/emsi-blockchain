699af8095585ab9caffea518d309d3ea69664513
Anas Trak <anastarak9@gmail.com>
2021-04-06 01:43:54 +0100

    inshaellah works

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -8,12 +8,9 @@
  * @digest: buffer for hash
  * Return: pointer to digest else NULL
  */
-uint8_t *sha256(int8_t const *s, size_t len,
-		uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!s || !digest)
 		return (NULL);
 	return (SHA256((const unsigned char *)s, len, d}
-
-
-				}
+}
