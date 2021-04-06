deba9b7117b791d5e8733aa5964336d66acfd1a6
Anas Trak <anastarak9@gmail.com>
2021-04-06 01:55:31 +0100

    inshaellah works

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,17 +1,17 @@
 #include "hblk_crypto.h"
 
 /**
- * sha256 - gets SHA256 hash for given string
- * @s: the string to encode
- * @len: the length of given string
- * @digest: buffer for hash
- * Return: pointer to digest else NULL
+ * sha256 - compute the hash of a sequence of bytes
+ * @s: Sequence of bytes to be hashed
+ * @len: Bytes to hash
+ * @digest: array to store resulting hash in
+ *
+ * Return: pointer to digest, NULL on error
  */
-uint8_t *sha256(int8_t const *s, size_t len,
-		uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t* sha256(int8_t const* s, size_t len,
+	uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!s || !digest)
 		return (NULL);
-	return (SHA256((const unsigned char *)s, len, digest));
+	return (SHA256((const unsigned char*)s, len, digest));
 }
-
