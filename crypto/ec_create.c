00243cc0b357daecbd3cc620c10682da9f48b319
AnasTrk <anastarak9@gmail.com>
2021-04-12 13:43:19 +0000

    message

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -4,19 +4,21 @@
  * ec_create - creates new private/public key pair
  * Return: pointer to EC_KEY or NULL
  */
-
 EC_KEY *ec_create(void)
 {
-	EC_KEY *key;
+        EC_KEY *key;
 
-	key = EC_KEY_new_by_curve_name(EC_CURVE);
-	if (!key)
-		return (NULL);
-	if (!EC_KEY_generate_key(key))
+        key = EC_KEY_new_by_curve_name(EC_CURVE);
+        if (!key)
 	{
-		EC_KEY_free(key);
-		return (NULL);
+		return(NULL);
 	}
-	return (key);
-}
 
+        if (!EC_KEY_generate_key(key))
+        {
+                EC_KEY_free(key);
+                return (NULL);
+        }
+
+        return (key);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -35,17 +35,13 @@ typedef struct sig_s
 	uint8_t	len;
 } sig_t;
 
-uint8_t* sha256(int8_t const* s, size_t len,
-	uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t* sha256(int8_t const* s, size_t len,uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY* ec_create(void);
 uint8_t* ec_to_pub(EC_KEY const* key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY* ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY* key, char const* folder);
 EC_KEY* ec_load(char const* folder);
-uint8_t* ec_sign(EC_KEY const* key, uint8_t const* msg, size_t msglen,
-	sig_t* sig);
-int ec_verify(EC_KEY const* key, uint8_t const* msg, size_t msglen,
-	sig_t const* sig);
+uint8_t* ec_sign(EC_KEY const* key, uint8_t const* msg, size_t msglen,sig_t* sig);
+int ec_verify(EC_KEY const* key, uint8_t const* msg, size_t msglen,sig_t const* sig);
 
 #endif /* HBLK_CRYPTO_H */
-
