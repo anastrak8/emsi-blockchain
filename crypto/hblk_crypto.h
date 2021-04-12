469a8358e75ebea3431bbd5eb3648523504b286b
AnasTrk <anastarak9@gmail.com>
2021-04-12 21:39:26 +0000

    m

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -9,7 +9,6 @@
 #include <string.h>
 #include <sys/stat.h>
 
-
 #define PUB_FILENAME	"key_pub.pem"
 #define PRI_FILENAME	"key.pem"
 #define EC_CURVE	NID_secp256k1
@@ -35,14 +34,16 @@ typedef struct sig_s
 	uint8_t	len;
 } sig_t;
 
-uint8_t* sha256(int8_t const* s, size_t len,uint8_t digest[SHA256_DIGEST_LENGTH]);
-EC_KEY* ec_create(void);
-uint8_t* ec_to_pub(EC_KEY const* key, uint8_t pub[EC_PUB_LEN]);
-EC_KEY* ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
-int ec_save(EC_KEY* key, char const* folder);
-EC_KEY* ec_load(char const* folder);
-uint8_t* ec_sign(EC_KEY const* key, uint8_t const* msg, size_t msglen,sig_t* sig);
-int ec_verify(EC_KEY const* key, uint8_t const* msg, size_t msglen,sig_t const* sig);
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH]);
+EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
+EC_KEY *ec_load(char const *folder);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+		sig_t *sig);
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+		sig_t const *sig);
 
 #endif /* HBLK_CRYPTO_H */
-
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -8,11 +8,10 @@
  *
  * Return: pointer to digest, NULL on error
  */
-uint8_t* sha256(int8_t const* s, size_t len,
-	uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!s || !digest)
 		return (NULL);
-	return (SHA256((const unsigned char*)s, len, digest));
+	return (SHA256((const unsigned char *)s, len, digest));
 }
-
