4165229c6cd13f3b9d9af01625bf3336f96a7b1e
root <root@Trak.localdomain>
2021-04-16 01:45:12 +0000

    m

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -19,31 +19,31 @@
 #define SIG_MAX_LEN	72
 
 /**
- * struct sig_s - EC Signature structure
- *
- * @sig: Signature buffer. The whole space may not be used
- * @len: Signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
- */
+   * struct sig_s - EC Signature structure
+    *
+     * @sig: Signature buffer. The whole space may not be used
+      * @len: Signature size. Can't exceed SIG_MAX_LEN, therefore stored on a byte
+       */
 typedef struct sig_s
 {
 	/*
-	 * @sig must stay first, so we can directly use the structure as
-	 * an array of char
-	 */
+	* @sig must stay first, so we can directly use the structure as
+	* an array of char
+	*/
 	uint8_t	sig[SIG_MAX_LEN];
 	uint8_t	len;
 } sig_t;
 
 uint8_t *sha256(int8_t const *s, size_t len,
-		uint8_t digest[SHA256_DIGEST_LENGTH]);
+				uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-		sig_t *sig);
+				sig_t *sig);
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-		sig_t const *sig);
+				sig_t const *sig);
 
 #endif /* HBLK_CRYPTO_H */
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,16 +1,16 @@
 #include "hblk_crypto.h"
 
 /**
- * sha256 - compute the hash of a sequence of bytes
- * @s: Sequence of bytes to be hashed
- * @len: Bytes to hash
- * @digest: array to store resulting hash in
- *
- * Return: pointer to digest, NULL on error
- */
+ * sha256-compute the hash of a sequence of bytes
+ *   * @s: Sequence of bytes to be hashed
+ *    * @len: Bytes to hash
+ *     * @digest: array to store resulting hash in
+ *      * @SHA256 - gooo
+ *       * Return: pointer to digest, NULL on error
+ *        */
 uint8_t *sha256(int8_t const *s, size_t len,uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-	if (!s || !digest)
-		return (NULL);
-	return (SHA256((const unsigned char *)s, len, digest));
+		if (!s || !digest)
+					return (NULL);
+			return (SHA256((const unsigned char *)s, len, digest));
 }
