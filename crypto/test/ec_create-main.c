65e9df85d1eb05b0aebcd1f3dc24ee8c7dcdf260
AnasTrk <anastarak9@gmail.com>
2021-04-17 21:03:43 +0000

    done

--- crypto/test/ec_create-main.c
+++ crypto/test/ec_create-main.c
@@ -11,6 +11,7 @@
 int main(void)
 {
     EC_KEY *key;
+
     /* Test `ec_create()` */
     key = ec_create();
     if (!key)
@@ -23,8 +24,11 @@ int main(void)
         fprintf(stderr, "Key verification failed\n");
         return (EXIT_FAILURE);
     }
+
     printf("Successfully created EC key pair\n");
+
     /* Cleanup */
     EC_KEY_free(key);
+
     return (EXIT_SUCCESS);
 }
